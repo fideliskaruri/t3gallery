f6e159385010d9c6e17741daa86551831c649fef
Fidelis Karuri <fkwachira@safaricom.co.ke>
2024-04-18 23:58:08 +0300

    added upload functionality

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -1,14 +1,24 @@
-import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/nextjs";
+"use client";
+
+import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from "@clerk/nextjs";
+import { UploadButton } from "~/utils/uploadthing";
+import { ourFileRouter } from "../api/uploadthing/core";
+import { useRouter } from "next/navigation";
 function TopNav() {
+    const router = useRouter();
+
     return (
         <nav className="flex w-full p-4 items-center justify-between text-xl border-b font-semibold ">
             <div>Gallery</div>
-            <div>
+            <div className="flex flex-row gap-2">
                 <SignedOut>
                     <SignInButton />
                 </SignedOut>
                 <SignedIn>
-<SignOutButton />
+                    <UploadButton endpoint="imageUploader" onClientUploadComplete={() => router.refresh() } />
+
+                    <UserButton />
+                    <SignOutButton />
                 </SignedIn>
             </div>
         </nav>
